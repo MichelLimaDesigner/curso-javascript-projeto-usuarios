f15e71d003dfe3cc492a0350522698b4de8747b4
MichelLimaDesigner <41237418+MichelLimaDesigner@users.noreply.github.com>
2019-01-11 17:41:32 -0200

    Criação da função getPhoto para cópia de imagens em base64

--- controller/UserController.js
+++ controller/UserController.js
@@ -17,11 +17,44 @@ class UserController {
                 padrão do evento, no caso, fazer refresh */
                 event.preventDefault();
 
-                this.addLine(this.getValues());
+                let values = this.getValues();
+
+                this.getPhoto((content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                });
 
             });
 
         } //fechando metodo onSubmit
+
+
+    getPhoto(callback) {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                callback(fileReader.result);
+
+            }
+
+            fileReader.readAsDataURL(file);
+
+        } // fechamento do metodo getPhoto
 
     getValues() {
 
@@ -63,7 +96,7 @@ class UserController {
             let tr = document.createElement("tr");
 
             tr.innerHTML = `
-    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
     <td>${dataUser.name}</td>
     <td>${dataUser.email}</td>
     <td>${dataUser.admin}</td>
