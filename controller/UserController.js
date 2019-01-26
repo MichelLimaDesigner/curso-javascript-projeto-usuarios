8aa6f4b245ddbbb0c70f5a8c3cfa3c8c431494d1
MichelLimaDesigner <41237418+MichelLimaDesigner@users.noreply.github.com>
2019-01-26 21:24:34 -0200

    Atualização em tempo real do número de cadastrados e usuário que são admins

--- controller/UserController.js
+++ controller/UserController.js
@@ -133,6 +133,8 @@ class UserController {
 
             let tr = document.createElement("tr");
 
+            tr.dataset.user = JSON.stringify(dataUser);
+
             tr.innerHTML = `
     <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
     <td>${dataUser.name}</td>
@@ -146,5 +148,26 @@ class UserController {
 
             this.tableEl.appendChild(tr);
 
+            this.updateCount();
+
         } //fechando metodo addLine
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmins = 0;
+
+        [...this.tableEl.children].forEach((e) => {
+
+            numberUsers++;
+
+            let user = JSON.parse(e.dataset.user);
+
+            if (user._admin) numberAdmins++;
+
+            document.getElementById("numberUsers").innerHTML = numberUsers;
+            document.getElementById("numberAdmins").innerHTML = numberAdmins;
+
+        });
+    }
 }
